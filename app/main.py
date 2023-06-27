f6fefe00947f8109d3e9e14cdef045ba67c5d75c
AleksandraV <am.amsti@gmail.com>
2023-06-27 15:31:16 +0300

    chore: vars rename

--- app/main.py
+++ app/main.py
@@ -3,10 +3,10 @@ from fastapi import FastAPI
 
 from app.config import get_config
 
-config = get_config()
+settings = get_config()
 
 app = FastAPI(title="Shortlink", description="Link shortener app")
 
 
 if __name__ == "__main__":
-    uvicorn.run('main:app', host=config.SERVER_HOST, port=config.SERVER_PORT, reload=True)
+    uvicorn.run('main:app', host=settings.SERVER_HOST, port=settings.SERVER_PORT, reload=True)
