d9460ef7cc00bd9e4edac48069b2b034cc0a1d93
AleksandraV <am.amsti@gmail.com>
2023-06-27 22:37:12 +0300

    fix: get_short_link return

--- app/service.py
+++ app/service.py
@@ -5,7 +5,7 @@ from app.database.models import LinkModel
 
 
 def create_unique_id(link_id: int, created_at: datetime) -> int:
-    return link_id + created_at.toordinal()
+    return int(str(link_id) + str(created_at.toordinal()))
 
 
 def create_short_url(unique_id: int) -> str:
@@ -23,4 +23,6 @@ def get_short_link(link_object: LinkModel) -> str:
     unique_id = create_unique_id(link_id=link_object.id, created_at=link_object.created_at)
     short_url_reversed = create_short_url(unique_id=unique_id)
 
-    return "".join(reversed(short_url_reversed))
+    result_short_link = f"example.com/{short_url_reversed[::-1]}"
+
+    return result_short_link
