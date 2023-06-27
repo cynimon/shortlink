b9d6342112e7f71b147dc743bbb42c4a200a9183
AleksandraV <am.amsti@gmail.com>
2023-06-27 16:00:34 +0300

    test: add tests to 3 endpoints

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,21 @@
+import pytest
+
 from faker import Faker
+
+from app.dependencies import get_db_session
 
 fake = Faker("en_US")
 
+
+@pytest.fixture
+def get_any_short_link() -> str:
+    db_session = next(get_db_session())
+
+    short_link = "gy"
+
+    return short_link
+
+
+@pytest.fixture
+def create_long_link() -> dict:
+    return {"url": fake.url()}
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -3,3 +3,31 @@ from fastapi.testclient import TestClient
 from app.main import app
 
 client = TestClient(app=app)
+
+
+def test_link_creation(create_long_link):
+    response = client.post(
+        url="/",
+        data=create_long_link
+    )
+
+    assert response.status_code == 201
+
+
+def test_long_link_getting(get_any_short_link):
+    response = client.get("/" + get_any_short_link)
+
+    assert response.status_code == 200
+
+
+def test_link_delete(get_any_short_link):
+    response = client.delete(
+        url="/" + get_any_short_link
+    )
+
+    assert response.status_code == 204
+
+    response = client.get(
+        url="/" + get_any_short_link
+    )
+    assert response.status_code == 404
