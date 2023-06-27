1f11d309873bf3f6a1825cb754c5cd04c759f29a
AleksandraV <am.amsti@gmail.com>
2023-06-27 23:32:22 +0300

    test: update endpoints

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,14 +7,14 @@ client = TestClient(app=app)
 
 def test_link_creation(create_long_link):
     response = client.post(
-        url="/",
+        url="/shortlink/",
         content=create_long_link
     )
 
     assert response.status_code == 201
 
     response = client.post(
-        url="/",
+        url="/shortlink/",
         content=create_long_link
     )
 
@@ -22,25 +22,25 @@ def test_link_creation(create_long_link):
 
 
 def test_long_link_getting_200(get_any_short_link):
-    response = client.get("/?short_url=" + get_any_short_link)
+    response = client.get("/shortlink/?short_url=" + get_any_short_link)
 
     assert response.status_code == 200
 
 
 def test_long_link_getting_404(get_any_short_link_fake):
-    response = client.get("/?short_url=" + get_any_short_link_fake)
+    response = client.get("/shortlink/?short_url=" + get_any_short_link_fake)
 
     assert response.status_code == 404
 
 
 def test_link_delete(get_any_short_link):
     response = client.delete(
-        url="/?short_url=" + get_any_short_link
+        url="/shortlink/?short_url=" + get_any_short_link
     )
 
     assert response.status_code == 204
 
     response = client.get(
-        url="/?short_url=" + get_any_short_link
+        url="/shortlink/?short_url=" + get_any_short_link
     )
     assert response.status_code == 404
